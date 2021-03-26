3cb69363242d6640f7fa5ddaaf1115e33a5f8ad6
niegrzybkowski <56119793+niegrzybkowski@users.noreply.github.com>
2021-03-26 16:38:25 +0100

    add script processing St. Antonius data

--- process_raw.R
+++ process_raw.R
@@ -23,14 +23,39 @@ process_tongji <- function(filename) {
     write.csv(file=str_c("data/", filename, ".csv"), row.names = F)
 }
 
-st_antonius_types = c("numeric", "text", "date", "date", "date",
-                      "text", "text", "date", "numeric", "date",
-                      "numeric", "numeric", "numeric", "numeric", "numeric")
+process_antonius <- function(filename) {
+  st_antonius_types = c("numeric", "text", "date", "date", "date",
+                        "text", "text", "date", "numeric", "date",
+                        "numeric", "numeric", "numeric", "numeric", "numeric")
 
-suppressWarnings( # expected warnings, caused by disgusting non-date strings in date columns
-  readxl::read_xlsx("raw_data/St_Antonius_NL.xlsx", col_types = st_antonius_types)
-  )
+  suppressWarnings( # expected warnings, caused by non-date strings in date columns
+    readxl::read_xlsx(str_c("raw_data/", filename, ".xlsx"), col_types = st_antonius_types)
+    ) %>%
+    mutate(id = 1:305) %>%
+    rename(
+      admission = "Date of admission",
+      discharge = "Date of discharge",
+      outcome = "Survival/death", 
+      LDH = "LD", 
+      hsCRP = "CRP", 
+      lymphocytes = "Percentage lymphocytes"
+    ) %>%
+    mutate(
+      LDH_first = na.omit(LDH),
+      LDH_last = na.omit(LDH),
+      hsCRP_first = na.omit(hsCRP),
+      hsCRP_last = na.omit(hsCRP),
+      lymphocytes_first = na.omit(lymphocytes),
+      lymphocytes_last = na.omit(lymphocytes),
+    ) %>%
+    select(id, admission, discharge, outcome, 
+           LDH_first, LDH_last, hsCRP_first, hsCRP_last, lymphocytes_first, lymphocytes_last) %>%
+    mutate(outcome = ifelse(outcome == "Alive", 0, 1))
+    write.csv(file = str_c("data/", filename, ".csv"), row.names = F)
+}
+  
 
 
 process_tongji("Tongji_375_CN")
 process_tongji("Tongji_110_CN")
+process_antonius("St_Antonius_NL")
