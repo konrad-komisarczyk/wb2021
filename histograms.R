504aaa5d275c195907864401061fee6cbc30811c
niegrzybkowski <56119793+niegrzybkowski@users.noreply.github.com>
2021-03-30 13:59:42 +0200

    formatted a bit

--- histograms.R
+++ histograms.R
@@ -1,26 +1,27 @@
-
 source("libraries.R")
 
 draw_histogram <- function(filename, which_LDH){
-df = data.frame(read.csv(str_c("data/", filename, ".csv")))
-
-ldh_min = 150
-ldh_max = 1000
-hist_scale_factor = 300
-title = sprintf('Plot of %s in %s',which_LDH, filename)
-
-df %>% mutate(died = case_when(
-  outcome == 1 ~ "died",
-  outcome == 0 ~ "survived"
-)) %>% ggplot(aes_string(x = which_LDH, fill = "died")) + geom_histogram(binwidth=30, alpha=0.3) +
-  scale_fill_manual(name='Outcome', labels=c("Died", "Survived"), values=c('blue', 'orange')) + xlim(ldh_min,ldh_max) + 
-  scale_y_continuous(name = "Number of patients") +
-  ggtitle(title) +
-  theme_bw() 
+  df = data.frame(read.csv(str_c("data/", filename, ".csv")))
+  
+  ldh_min = 150
+  ldh_max = 1000
+  hist_scale_factor = 300
+  title = sprintf('Plot of %s in %s', which_LDH, filename)
+  
+  df %>% 
+    mutate(
+      died = case_when(
+        outcome == 1 ~ "died",
+        outcome == 0 ~ "survived"
+    )) %>% 
+    ggplot(aes_string(x = which_LDH, fill = "died")) + 
+    geom_histogram(binwidth=100, alpha=0.3) +
+    scale_fill_manual(name='Outcome', labels=c("Died", "Survived"), values=c('blue', 'orange')) + 
+    xlim(ldh_min, ldh_max) + 
+    scale_y_continuous(name = "Number of patients") +
+    ggtitle(title) +
+    theme_bw() 
 }
-
-
-
 
 draw_histogram("Tongji_375_CN", "LDH_first")
 draw_histogram("Outcomerea_FR", "LDH_first")
@@ -30,3 +31,4 @@ draw_histogram("Tongji_375_CN", "LDH_last")
 draw_histogram("Outcomerea_FR", "LDH_last")
 draw_histogram("Tongji_110_CN", "LDH_last")
 draw_histogram("St_Antonius_NL", "LDH_last")
+
