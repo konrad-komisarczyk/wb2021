0d7d5c276320e6e0a673bd2167488169de33320e
niegrzybkowski <56119793+niegrzybkowski@users.noreply.github.com>
2021-04-03 13:15:49 +0200

    format lib.R file

--- libraries.R
+++ libraries.R
@@ -1,12 +1,18 @@
+# Basics
 library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(stringr)
+
+# Models
 library(xgboost)
 library(dbscan)
-
-#library(DataExplorer)
+library(DataExplorer)
 #devtools::install_github('ModelOriented/treeshap')
 library(treeshap)
 #library(caret)
+
+# EDA
+library(patchwork)
+library(gridExtra)
 
